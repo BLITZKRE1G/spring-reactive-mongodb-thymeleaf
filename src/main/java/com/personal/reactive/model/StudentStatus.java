76e62af9fa80ed6904d3397623e90e24947dba7e
Abhinab <abhinab_gupta@thbs.com>
2023-04-29 20:11:29 +0530

    added annotations

--- src/main/java/com/personal/reactive/model/StudentStatus.java
+++ src/main/java/com/personal/reactive/model/StudentStatus.java
@@ -1,15 +1,13 @@
 package com.personal.reactive.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum StudentStatus {
     ENROLLED("ENROLLED"),
     TERMINATED("TERMINATED"),
     SHORTLISTED("SHORTLISTED");
     private final String status;
-
-    StudentStatus(String status) {
-        this.status = status;
-    }
 }
